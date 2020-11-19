6828f06e1f75be5af422e731e0288a371603b14a
Aytac Malkoc <37803401+aytacmalkoc@users.noreply.github.com>
2020-11-20 00:14:45 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,36 +4,58 @@ namespace C_Sharp_Matris
 {
     class Program
     {
+        static void setColorGreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        static void resetColor()
+        {
+            Console.ResetColor();
+        }
+
         static void Main(string[] args)
         {
             int[,] dizi = new int[3, 3];
             int det = 0;
 
+            Console.Title = "C# Matris Generator v1.0";
+
+            setColorGreen();
+            Console.WriteLine("   ---------------------------------   ");
+            Console.Write("--- Welcome to matris generator v1.0 ---\n");
+            Console.WriteLine("   ---------------------------------   \n");
+            Console.WriteLine("@author: Aytac Malkoc - <iletisim@aytacmalkoc.com.tr>\n");
+
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    Console.Write("[{0}],[{1}] değeri: ", i, j);
+                    resetColor();
+                    Console.Write("value of ");
+                    setColorGreen();
+                    Console.Write("[{0}],[{1}]: ", i, j);
+                    resetColor();
                     dizi[i, j] = int.Parse(Console.ReadLine());
                 }
             }
             Console.WriteLine();
 
-            Console.WriteLine("Oluşturulan Matris: \n");
+            Console.WriteLine("Generated Matris: \n");
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                     Console.Write(" {0} ", dizi[i, j]);
-                    Console.WriteLine();
+                Console.WriteLine();
             }
             Console.WriteLine();
 
-            Console.WriteLine("Transpoz: \n");
+            Console.WriteLine("Transpoze: \n");
             for (int k = 0; k < 3; k++)
             {
                 for (int m = 0; m < 3; m++)
                     Console.Write(" {0} ", dizi[m, k]);
-                    Console.WriteLine();
+                Console.WriteLine();
             }
             Console.WriteLine();
 
